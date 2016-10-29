9e04048d3131a699ec85272836991cb17feb31f1
taykuz <zaycrix05@gmail.com>
2016-10-29 09:56:02 -0400

    nuevo1

---
+++ ejemplo7.js
@@ -0,0 +1,7 @@
+console.log("typeof 7 => "+ typeof 7)//=>"number"
+console.log("typeof \"hola\" => "+ typeof "hola")//=>"string"
+console.log("typeof true =>"+ typeof true)//=>boolean
+console.log("typeof undefined =>"+ typeof undefined)//=>"undefined"
+console.log("typeof null =>"+ typeof null)//"object"
+console.log("typeof new date =>"+ typeof new Date())//=>"date"
+console.log("typeof new Function() =>"+ typeof new Function())//=>"Function"
