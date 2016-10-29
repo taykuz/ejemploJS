f99b39001f2a30db5a47275c94f6ca19e39ce2be
taykuz <zaycrix05@gmail.com>
2016-10-29 09:20:49 -0400

    nuevo

---
+++ concatenar.js
@@ -0,0 +1,7 @@
+console.log("La suma de 3+7 es:" + (3+7) + "Signo de un numero mas 13:" + (+13) + "Concatenacion de string \"Hola\"*\"Pepe\":" + ("Hola"+"Pepe") );
+/*console.log("La suma de 3+7 es:");
+console.log(3+7);
+console.log("Signo de un numero mas 13:");
+console.log(+13);
+console.log("Concatenacion de string \"Hola\"*\"Pepe\":");
+console.log("Hola"+"Pepe");*/
