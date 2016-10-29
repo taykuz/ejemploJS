f0878b8e138d518fa77477fce939a88f0e057e4a
taykuz <zaycrix05@gmail.com>
2016-10-29 08:45:59 -0400

    iniciando repositorio

---
+++ ejemplo5.js
@@ -0,0 +1,14 @@
+/*console.log("Suma de Dos numeros");
+console.log (3+7);
+console.log("Resta de los numeros 13-7:");
+console.log(13-7);
+console.log("expresion con parentesis (8*2-4)/3:");
+console.log((8*2-4)/3);*/
+console.log("\"Hola\"");
+console.log("La suma de 3+7 es:");
+console.log(3+7);
+console.log("Signo de un numero mas 13:");
+console.log(+13);
+console.log("Concatenacion de string \"Hola\"*\"Pepe\":");
+console.log("Hola"+"Pepe");
+
---
+++ erjemplo4.js
@@ -0,0 +1,6 @@
+console.log("Suma de Dos numeros"+(3+7);
+console.log );
+console.log("Resta de los numeros 13-7:");
+console.log(13-7);
+console.log("expresion con parentesis (8*2-4)/3:");
+console.log((8*2-4)/3);
